bd7ce15b73294fcd931707d83b87d8374e04ea39
mbpiner <31866428+mbpiner@users.noreply.github.com>
2023-09-10 19:37:22 -0400

    Update InHousePart.java

--- src/model/InHousePart.java
+++ src/model/InHousePart.java
@@ -1,38 +1,39 @@
 package model;
+
 public class InHousePart extends Part {
-    
+
     private int machineID;
-    
-    public InHousePart(int partID, String name, int stock, int min, int max, double price, int machineID){
-        
-        
+
+    public InHousePart(int partID, String name, int stock, int min, int max, double price, int machineID) {
+
+
         super(partID, name, stock, min, max, price);
-        
+
         this.setMachineID(machineID);
-        
+
     }
-        
-        
-        public void setMachineID(int machineID){
-            
-            this.machineID = machineID; 
-        
-        }
-        
-        
-        public int getMachineID(){
-    
-            return this.machineID;
-    
-        }
-        
-    
+
+
+    public void setMachineID(int machineID) {
+
+        this.machineID = machineID;
+
     }
-    
-    
-    
-    
-    
-    
-    
-   
+
+
+    public int getMachineID() {
+
+        return this.machineID;
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
