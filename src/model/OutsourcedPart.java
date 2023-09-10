511d64f84f102220998abfd8681a592f288ea49e
mbpiner <31866428+mbpiner@users.noreply.github.com>
2023-09-10 19:39:34 -0400

    Update OutsourcedPart.java

--- src/model/OutsourcedPart.java
+++ src/model/OutsourcedPart.java
@@ -3,41 +3,38 @@ package model;
 
 
 public class OutsourcedPart extends Part {
-    
+
     private String companyName;
-    
-    public OutsourcedPart(int partID, String name, int stock, int min, int max, double price, String companyName){
-        
-        
+
+    public OutsourcedPart(int partID, String name, int stock, int min, int max, double price, String companyName) {
+
+
         super(partID, name, stock, min, max, price);
         this.setCompanyName(companyName);
-        
-         
+
+
     }
-        
-        
-        public void setCompanyName(String companyName){
-            this.companyName = companyName;
-            
-        
-        }
-        
-        
-        public String getCompanyName(){
-            return this.companyName;
-    
-        }
-        
-        
-        
-    
-    
+
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+
+
     }
-    
-    
-    
-    
-    
-    
-    
-   
+
+
+    public String getCompanyName() {
+        return this.companyName;
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
