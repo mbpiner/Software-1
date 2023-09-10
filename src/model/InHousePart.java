c93ede9b5232e3da21b1aba355e4cfa5f2eb9510
mbpiner <31866428+mbpiner@users.noreply.github.com>
2023-09-10 19:12:28 -0400

    Update InHousePart.java

--- src/model/InHousePart.java
+++ src/model/InHousePart.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
-
-/**
- *
- * @author Mason
- */
 public class InHousePart extends Part {
     
     private int machineID;
@@ -17,20 +7,15 @@ public class InHousePart extends Part {
         
         
         super(partID, name, stock, min, max, price);
+        
         this.setMachineID(machineID);
-        
-        
-        
-     
-        
         
     }
         
         
         public void setMachineID(int machineID){
             
-            this.machineID = machineID;
-            
+            this.machineID = machineID; 
         
         }
         
@@ -41,9 +26,6 @@ public class InHousePart extends Part {
     
         }
         
-        
-        
-    
     
     }
     
